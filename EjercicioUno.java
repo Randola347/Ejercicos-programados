44e0a2c2430c10df98b601b2b4582a4e1aa63040
Randall Madrigal <lpperezo84@gmail.com>
2023-05-18 10:58:09 -0600

    Se agrega el mensaje nuevo

--- EjercicioUno.java
+++ EjercicioUno.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
-public class Ejercicio1{
+public class EjercicioUno{
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        System.out.println("Ingrese un numerillo: ");
+        System.out.println("Ingrese un numero: ");
         int num = input.nextInt();
         if (num%2==0){
             System.out.println("Es par ");
