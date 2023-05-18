1b010d3bbc8011728c25c7f50691fd890ba602ca
Randall Madrigal <lpperezo84@gmail.com>
2023-05-18 12:44:18 -0600

    Se agregan nuevos proyectos

---
+++ EjercicioCuatro.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class EjercicioCuatro {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingresa el primer número: ");
+        int numero1 = scanner.nextInt();
+
+        System.out.println("Ingresa el segundo número: ");
+        int numero2 = scanner.nextInt();
+
+        int suma = numero1 + numero2;
+        int resta = numero1 - numero2;
+        int division = numero1 / numero2;
+        int multiplicacion = numero1 * numero2;
+
+        System.out.println("Suma: " + suma);
+        System.out.println("Resta: " + resta);
+        System.out.println("División: " + division);
+        System.out.println("Multiplicación: " + multiplicacion);
+    }
+}
