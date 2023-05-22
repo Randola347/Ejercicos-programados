5a93a6ea94c1e6515dc9169efeba792650a3a866
Randola347 <lpperezo84@gmail.com>
2023-05-22 10:07:02 -0600

    Arreglo de elevacion

--- EjercicioTres.java
+++ EjercicioTres.java
@@ -3,23 +3,24 @@ import java.util.Scanner;
 public class EjercicioTres {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+       Double PI= 3.14159;
 
         System.out.println("Ingresa el radio del círculo: ");
-        double radio = scanner.nextDouble();
-        double area = calcularAreaCirculo(radio);
-        double perimetro = calcularPerimetroCirculo(radio);
+        double radio = scanner.nextFloat();
+        double area = calcularAreaCirculo(radio,PI);
+        double perimetro = calcularPerimetroCirculo(radio,PI);
 
         System.out.println("El área del círculo es: " + area);
         System.out.println("El perímetro del círculo es: " + perimetro);
     }
 
-    public static double calcularAreaCirculo(double radio) {
-        double area = Math.PI * Math.pow(radio,2);
+    public static double calcularAreaCirculo(double radio,Double pi) {
+        Double area =  pi*radio * radio;
         return area;
     }
 
-    public static double calcularPerimetroCirculo(double radio) {
-        double perimetro = 2 * Math.PI * radio;
+    public static double calcularPerimetroCirculo(double radio,Double pi) {
+        Double perimetro = 2 * pi * radio;
         return perimetro;
     }
 }
